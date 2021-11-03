4fd55e426fbf1d4cdeb3ba09b77be70e56def89f
Moin-git-bit <92537244+Moin-git-bit@users.noreply.github.com>
2021-11-03 21:27:53 +0530

    Add files via upload

---
+++ 01 program to print fork infosystem 5 times.c
@@ -0,0 +1,20 @@
+/// program to print fork infosystem 5 time ///
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int cnt = 0;
+
+          printf("\n we are printing fork infosystem 5 times = ");
+
+    for(cnt = 1;cnt <= 5;cnt++)
+    {
+          printf("\n\n fork \n");
+          printf("\n\n infosystem \n ");
+    }
+
+         getch();
+         return 0;
+}
